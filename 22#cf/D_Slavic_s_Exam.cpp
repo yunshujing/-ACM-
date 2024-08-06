839eb483dd9e0a08b20ec8f8d4e2a3602bf7481c
云书景 <143301977+yunshujing@users.noreply.github.com>
2024-08-07 01:06:17 +0800

    080701

--- 22#cf/D_Slavic_s_Exam.cpp
+++ 22#cf/D_Slavic_s_Exam.cpp
@@ -1,31 +1,37 @@
-#include <iostream>  
-#include <string>  
-#include <vector>  
-  
-using namespace std;  
-  
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl "\n"
+const int N = 1e6 + 10;
+const double eps =1e-4;
+
 string canFormSubsequence(const string& s, const string& t) {  
     string result = s; // 复制s到result，以便修改  
-    int j = 0; // t的指针  
+    int j = 0; // t的指针
+    bool flag = 0;
     for (int i = 0; i < result.size() && j < t.size(); ++i) {  
         if (result[i] == '?' || result[i] == t[j]) {  
             // 如果result[i]是'?'或者已经匹配上了t[j]，则替换'?'为t[j]（如果它是'?'的话）  
             if (result[i] == '?') {  
                 result[i] = t[j];  
             }  
-            ++j; // 移动t的指针  
+            ++j; // 移动t的指针
+            if(j==t.size()){
+                j = 0;
+                flag = 1;
+            }
         }  
         // 如果result[i]不是'?'且不等于t[j]，则继续遍历result，不需要替换  
     }  
     // 检查是否所有的t中的字符都被匹配了  
-    if (j == t.size()) {  
+    if (flag) {  
         return "YES\n" + result;  
     } else {  
         return "NO";  
     }  
 }  
   
-int main() {  
+signed main() {  
     int T;  
     cin >> T;  
     cin.ignore(); // 忽略T后面的换行符  
