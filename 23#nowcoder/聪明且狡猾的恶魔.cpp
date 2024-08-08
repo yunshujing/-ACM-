ea78cdf68607e1c67efa03f9dfac685689e49b5f
云书景 <143301977+yunshujing@users.noreply.github.com>
2024-08-08 13:32:44 +0800

    0808

--- 23#nowcoder/聪明且狡猾的恶魔.cpp
+++ 23#nowcoder/聪明且狡猾的恶魔.cpp
@@ -2,15 +2,8 @@
 using namespace std;
 #define int long long
 #define endl "\n"
-#define vll vector<long long>
-#define vld vector<long double>
-#define ff first
-#define ss second
-#define pb push_back
 const int N = 1e6 + 10;
 const double eps =1e-4;
-const int mod1 = 998244353;
-
 //int a[N];
 //vector <int> a;
 
@@ -18,7 +11,16 @@ signed main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);cout.tie(nullptr);
     
-    
+    int t;
+    cin >> t;
+    while(t--){
+        int x, n;
+        cin >> x >> n;
+
+        int num1 = (n-1) / 2;
+        x -= num1;
+        cout << x << endl;
+    }
     
     return 0;
 }
--- 23#nowcoder/该出奇兵了.cpp
+++ 23#nowcoder/该出奇兵了.cpp
@@ -2,23 +2,30 @@
 using namespace std;
 #define int long long
 #define endl "\n"
-#define vll vector<long long>
-#define vld vector<long double>
-#define ff first
-#define ss second
-#define pb push_back
 const int N = 1e6 + 10;
 const double eps =1e-4;
-const int mod1 = 998244353;
-
-//int a[N];
+int b[N][2];
+int a[N];
 //vector <int> a;
 
 signed main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);cout.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++)
+    {
+        cin >> b[i][0] >> b[i][1];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
     
-    
-    
+
+
     return 0;
 }
