46bb2e02683d27d97bab55124860f436eacc2576
云书景 <143301977+yunshujing@users.noreply.github.com>
2024-09-08 00:02:38 +0800

    0908

--- abc/0907/C_Word_Ladder.cpp
+++ abc/0907/C_Word_Ladder.cpp
@@ -8,18 +8,6 @@ using namespace std;
 const int N = 1e6 + 10;
 const double eps =1e-4;
 
-struct Step
-{
-    char zm;
-    int zb;
-};
-
-Step step2[110];
-
-bool cmp(Step a, Step b){
-    return a.zm < b.zm;
-}
-
 signed main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
@@ -28,10 +16,8 @@ signed main(){
 
     string s, t;
     vector<string> x;
-    int flag = 1;
     cin >> s >> t;
     int len = s.size();
-    int c = 0;
     for (int i = 0; i < len;i++){
         if(s[i]>t[i]){
             string ss1 = s.substr(0, i);
@@ -40,39 +26,22 @@ signed main(){
             string sss = ss1 + ss2 + ss3;
             x.push_back(sss);
             s = sss;
-            flag= 0;
-        }
-        else if(s[i]!=t[i]){
-            step2[c].zm = t[i];
-            step2[c].zb = i;
-            c++;
-            flag = 0;
         }
     }
-    if(flag){
-        cout << "0" << endl;
-        return 0;
+    for (int i = len-1; i >= 0;i--){
+            if(s[i]<t[i]){
+                string ss1 = s.substr(0, i);
+                char ss2 = t[i];
+                string ss3 = s.substr(i+1);
+                string sss = ss1 + ss2 + ss3;
+                x.push_back(sss);
+                s = sss;
+            }
+        }
+    cout << x.size() << endl;
+    for (int i = 0; i < x.size(); i++){
+        cout << x[i] << endl;
     }
 
-    sort(step2, step2+c, cmp);
-    for (int i = 0; i < c;i++){
-        int j = step2[i].zb;
-        string ss1 = s.substr(0, j);
-        char ss2 = step2[i].zm;
-        string ss3 = s.substr(j+1);
-        string sss = ss1 + ss2 + ss3;
-        x.push_back(sss);
-    }
-    
-    int cnt = 0;
-    
-    if(!x.empty()){
-        cnt = x.size();
-        cout << cnt << endl;
-        
-        for (int i = 0; i < cnt;i++){
-            cout << x[i] << endl;
-        }
-    }
     return 0;
 }
